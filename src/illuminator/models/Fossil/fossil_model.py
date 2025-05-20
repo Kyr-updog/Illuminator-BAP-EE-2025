4299930db58cd58fa3ed4f3d1487fd3f2b1fddde
Kyr-updog <krahimatulla@gmail.com>
2025-05-20 13:42:20 +0200

    Update fossil_model.py and nuclear_model.py

--- src/illuminator/models/Fossil/fossil_model.py
+++ src/illuminator/models/Fossil/fossil_model.py
@@ -1,36 +1,38 @@
-# Code to model fossil fuel generators
-
 from illuminator.builder import ModelConstructor
 
 # construct the model
 class Fossil(ModelConstructor):
-    # Define the model parameters, inputs, outputs...
-    # all parameters will be directly available as attributes
-    parameters={'fos_type': 'coal',         # Type of fossil fuel (also determines type of biomass): coal, oil, gas.
-                'fos_eff': 1,               # Fraction mechanical energy from chemical energy from the fossil fuel.
-                'bio_eff': 1,               # Fraction mechanical energy from chemical energy from the biomass.
+    # Define the model parameters, inputs, outputs, and states
+    # Energy conversions:
+    # Chemical -> exhaust thermal: inefficiency possible (imperfect combustion; could be different between fossil fuel and biomass)
+    # Exhaust thermal -> steam thermal: inefficiency possible (waste heat)
+    # Steam thermal -> mechanical pressure: inefficiency possible (waste heat)
+    # Mechanical pressure -> rotational: inefficiency possible (friction)
+    # Rotational -> electrical: inefficiency possible (waste flux)
+    parameters={'fos_type': 'coal',         # Type of fossil fuel (also determines type of biomass): coal, oil, gas (solid, liquid, gas).
+                'fos_eff': 1,               # Fraction electrical energy from chemical energy from fossil fuel.
+                'bio_eff': 1,               # Fraction electrical energy from chemical energy from biomass.
                 'fos_NRG_density': 1,       # Amount of chemical energy per unit of weight (kWh/kg) for the fossil fuel.
                 'bio_NRG_density': 1,       # Amount of chemical energy per unit of weight (kWh/kg) for the biomass.
                 'fos_specific_emission': 1, # Amount of CO2 emission (kg) per unit of weight (kg) of burned fossil fuel.
                 'bio_specific_emission': 1, # Amount of CO2 emission (kg) per unit of weight (kg) of burned biomass.
-                'max_pow': 500,             # Maximum power generation capacity (kW).
+                'rated_pow': 500,             # Maximum power generation capacity (kW).
                 'output_type': 'power'      # Output type of the generation, either 'power' (kW) or 'energy' (kWh).
                 }
-    inputs={'req_pow': 0    # Required power output (kW) or energy (kWh) based on the chosen output type (power or energy).
-                            # Alternatively, use tot_pow and let the generator calculate the output.
+    inputs={'req_pow': 0,       # Required power output (kW) or energy (kWh) based on the chosen output type (power or energy).
+            'bio_frac': 0.2     # Fraction of mass per second ((kg/s)/(kg/s)) burned that is biomass for a given hour.
             }
-    outputs={'gen_pow': 0,  # Generated power output (kW) or energy (kWh) based on the chosen output type (power or energy).
-             'emission': 0  # Amount of CO2 emission (kg) for that time step.
+    outputs={'gen_pow': 0,          # Generated power output (kW) or energy (kWh) based on the chosen output type (power or energy).
+             'emission': 0,         # Amount of CO2 emission (kg) for that time step.
+             'fossil_fuel_burned': 0, # Amount of fossil fuel (kg) used for that time step.
+             'biomass_burned': 0      # Amount of biomass (kg) used for that time step.
              }
-    states={'bio_frac': 0.2,        # Fraction of mass per second (kg/s) burned that is biomass for a given hour.
-            'price': 10,            # Price per unit of energy (euros/kWh) for a given hour.
-            'committed_NRG': 1000   # Amount of energy (kWh) committed for a given hour.
+    states={'limit_flag': 0         # 0: req_pow within rated power capacity; 1: req_pow greater than rated power capacity
             }
     
     # define other attributes
     time_step_size=1
     time=None
-    powerout = 0  # Output power for given required power.
 
     def __init__(self, **kwargs) -> None:
         """
@@ -49,13 +51,10 @@ class Fossil(ModelConstructor):
         self.bio_NRG_density = self.parameters['bio_NRG_density']
         self.fos_specific_emission = self.parameters['fos_specific_emission']
         self.bio_specific_emission = self.parameters['bio_specific_emission']
-        self.max_pow = self.parameters['max_pow']
+        self.rated_pow = self.parameters['rated_pow']
         self.output_type = self.parameters['output_type']
-        self.bio_frac = self.states['bio_frac']
-        self.price = self.states['price']
-        self.committed_NRG = self.states['committed_NRG']
-         
 
+         
 
     # define step function
     def step(self, time: int, inputs: dict=None, max_advance: int=1) -> None:  # step function always needs arguments self, time, inputs and max_advance. Max_advance needs an initial value.
@@ -69,48 +68,61 @@ class Fossil(ModelConstructor):
         Returns:
             float: Next simulation time in seconds
         """
-        self.resolution_h = self.time_resolution / 60 / 60  # convert scenario resolution to hours
         input_data = self.unpack_inputs(inputs)  # make input data easily accessible
+        self.time = time
 
-        results = self.generation(req_pow=input_data['req_pow'])
+        req_pow = input_data['req_pow']
+        bio_frac = input_data['bio_frac']
 
+        results = self.generation(req_pow, bio_frac)
+        self.limit_flag = results.pop('limit_flag')
+
+        self.set_states({'limit_flag': self.limit_flag})
         self.set_outputs(results)
-        self.set_states({'bio_frac': self.bio_frac})
-        self.set_states({'price': self.price})
-        self.set_states({'committed_NRG': self.committed_NRG})
 
         # return the time of the next step (time until current information is valid)
         return time + self._model.time_step_size
     
 
 
-    def frac(n1, n2, d1):
+    def pow_frac(n1, n2, d1):
         frac = n1*n2/(n1*n2 + (1-n1)*d1)
         return frac
     
-
     
-    def emission_rate(mech_pow, eff, NRG_density, specific_emission) -> float:
+    
+    def burn_and_emission_rate(mech_pow, eff, NRG_density, specific_emission) -> float:
         # Calculate CO2 emission rate (kg/s)
         chem_pow = mech_pow/eff
         burn_rate = chem_pow/NRG_density
         emission_rate = burn_rate*specific_emission
-        return emission_rate
+        return burn_rate, emission_rate
     
 
 
-    def generation(self, req_pow:float) -> dict:
+    def generation(self, req_pow:float, bio_frac:float) -> dict:
+        if req_pow > self.rated_pow:
+            limit_flag = 1
+        else:
+            limit_flag = 0
+
         # Principle: gen_pow == req_pow
-        bio_chem_pow_frac = self.frac(self.bio_frac, self.bio_NRG_density, self.fos_NRG_density)
-        bio_mech_pow_frac = self.frac(bio_chem_pow_frac, self.bio_eff, self.fos_eff)
+        bio_chem_pow_frac = self.pow_frac(bio_frac, self.bio_NRG_density, self.fos_NRG_density)
+        bio_mech_pow_frac = self.pow_frac(bio_chem_pow_frac, self.bio_eff, self.fos_eff)
         
         bio_mech_pow = req_pow*bio_mech_pow_frac
         fos_mech_pow = req_pow - bio_mech_pow
 
-        fos_emission_rate = self.emission_rate(fos_mech_pow, self.fos_eff, self.fos_NRG_density, self.fos_specific_emission)
-        bio_emission_rate = self.emission_rate(bio_mech_pow, self.bio_eff, self.bio_NRG_density, self.bio_specific_emission)
+        fos_burn_rate, fos_emission_rate = self.burn_and_emission_rate(fos_mech_pow, self.fos_eff,
+                                                                       self.fos_NRG_density, self.fos_specific_emission)
+        bio_burn_rate, bio_emission_rate = self.burn_and_emission_rate(bio_mech_pow, self.bio_eff,
+                                                                       self.bio_NRG_density, self.bio_specific_emission)
+        
+        fossil_fuel_burned = fos_burn_rate * self.time_step_size
+        biomass_burned = bio_burn_rate * self.time_step_size
 
         emission = (fos_emission_rate + bio_emission_rate) * self.time_step_size
 
-        re_params = {'gen_pow': req_pow, 'emission': emission}
+        re_params = {'gen_pow': req_pow, 'emission': emission, 'fossil_fuel_burned': fossil_fuel_burned,
+                     'biomass_burned': biomass_burned, 'limit_flag': limit_flag}
         return re_params
