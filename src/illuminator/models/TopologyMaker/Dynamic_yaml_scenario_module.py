06eb860fd05600873e95d209774d9ad6997ea853
amvermeer <152719164+amvermeer@users.noreply.github.com>
2025-06-01 23:05:02 +0200

    Update Dynamic_yaml_scenario_module.py
    Adjusted to include a third yaml connection, the line_id. 
    
    Done by adding the value of the ID to each connected pair list, which is then added to a third column of connected_pair_array.

--- src/illuminator/models/TopologyMaker/Dynamic_yaml_scenario_module.py
+++ src/illuminator/models/TopologyMaker/Dynamic_yaml_scenario_module.py
@@ -6,21 +6,22 @@ def determine_connected_pairs(Network):           #this function creates an arra
 
     highest_ID = np.max(np.array(np.array(Network)[:,0], dtype = int))  #determine highest ID value in the Network
     ID = np.arange(1, highest_ID + 1)                                   #create ID list with range 1 up to highest ID + 1
-    connected_pair_array = np.empty((0, 2))                             #create a blank-by-2 array  
+    connected_pair_array = np.empty((0, 3))                             #create a blank-by-2 array  
     for i in np.arange(len(ID)):                                        #loop over every ID value
         connected_pair = []                                             #redefine connected_pair list before each ID value
         for model in np.arange(len(Network)):                           #loop over the Network array
             if Network[model][0] == ID[i]:                              #compare Network's 0th element to ID's ith element
                 connected_pair.append(Network[model][1])                #add Network's Station element to the connected_pair list
                 connected_pair.append(Network[model][2])                #add Network's Sender/Receiver element to list
+                connected_pair.append(i+1)
         if (len(connected_pair) == 0):                                  #if this ID value had no connections, skips
             continue
         
         if ('Sender' in connected_pair[1]):
-            connected_pair_array = np.append(connected_pair_array, [[connected_pair[0], connected_pair[2]]], axis=0)
+            connected_pair_array = np.append(connected_pair_array, [[connected_pair[0], connected_pair[3], connected_pair[2]]], axis=0)
             #add connected Station pair to next row in array in standard order (for S/R) 
         else:    
-            connected_pair_array = np.append(connected_pair_array, [[connected_pair[2], connected_pair[0]]], axis=0) 
+            connected_pair_array = np.append(connected_pair_array, [[connected_pair[3], connected_pair[0], connected_pair[2]]], axis=0) 
             #add connected Station pair to next row in array in reverse order (for S/R)                        
     return connected_pair_array
 
@@ -30,12 +31,14 @@ def write_topology(connected_pair_array):
     topology_list = []   
     from_model = ''
     to_model = ''
+    line_id = ''
 
     for i in range(np.shape(connected_pair_array)[0]):
         if ('Station' in connected_pair_array[i, 0]) and ('Station' in connected_pair_array[i, 1]):
             from_model = connected_pair_array[i, 0]+'.transmit'
             to_model = connected_pair_array[i, 1]+'.receive'
-            topology_list.append({'from': from_model, 'to': to_model},)
+            line_id = 'line_'+connected_pair_array[i, 2]
+            topology_list.append({'from': from_model, 'to': to_model, 'line_id': line_id})
     return topology_list
 
 
@@ -66,10 +69,4 @@ if __name__ == "__main":
     print(connected_pair_array)
     topology = write_topology(connected_pair_array)
     print (topology)
-
-    '''topology = [ {'from': 'CSVload.load', 'to': 'Load1.load'},
-                {'from' : 'PV', 'to': 'Controller'}, 
-                {'from' : 'Wind', 'to': 'Home'}, 
-                {'from' : 'Wind', 'to': 'Controller'}
-    ]   '''
     read_and_copy_yaml_data_plus_add_data_to_new_file('Tutorial_Power_Balance_b', 'simulation_file')
