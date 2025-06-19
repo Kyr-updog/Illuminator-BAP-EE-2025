cd0d6844d77a3ff25c48d79d35b45215990a589c
Kyr-updog <krahimatulla@gmail.com>
2025-06-19 12:31:22 +0200

    Update pv_model_v3.py

--- src/illuminator/models/PV/pv_model_v3.py
+++ src/illuminator/models/PV/pv_model_v3.py
@@ -1,6 +1,7 @@
 from illuminator.builder import IlluminatorModel, ModelConstructor
 import numpy as np
 import mosaik_api_v3 as mosaik_api
+from scipy.stats import laplace
 
 # construct the model
 class PV(ModelConstructor):
@@ -48,7 +49,11 @@ class PV(ModelConstructor):
         'm_az': 0,
         'cap': 0,  # installed capacity
         'output_type': 'power',
-        'sim_start': 0
+        'sim_start': 0,
+        'input_type': 'percentage', # 'irradiation' or 'percentage' 
+        'name': 'PV1',
+        'par1': 0,
+        'par2': 0
         }
     inputs={
         "G_Gh": 0,  # Global Horizontal Irradiance (GHI) in W/m², representing the total solar radiation received on a horizontal surface.
@@ -57,14 +62,16 @@ class PV(ModelConstructor):
         "Ta": 0,  # Ambient temperature (°C) of the environment surrounding the PV panels.
         "hs": 0,  # Solar elevation angle (degrees), indicating the height of the sun in the sky.
         "FF": 0,  # Wind speed (m/s), which affects the temperature and performance of the PV panels.
-        "Az": 0  # Sun azimuth angle (degrees), indicating the sun's position in the horizontal plane.
+        "Az": 0,  # Sun azimuth angle (degrees), indicating the sun's position in the horizontal plane.
+        "capacity_percentage": 0
         }
     outputs={
-        "pv_gen": 0,  # Generated PV power output (kW) or energy (kWh) based on the chosen output type (power or energy).
+        "pv_gen_out": 0,  # Generated PV power output (kW) or energy (kWh) based on the chosen output type (power or energy).
         "total_irr": 0,  # Total irradiance (W/m²) received on the PV module, considering direct, diffuse, and reflected components.
         "g_aoi": 0  # Total irradiance (W/m²) accounting for angle of incidence, diffuse, and reflected irradiance.
         }
-    states={'pv_genState': 0
+    states={'pv_genState': 0,
+            'pv_gen': {}
             }
     time_step_size=1
     time=None
@@ -89,6 +96,10 @@ class PV(ModelConstructor):
         self.m_az = self._model.parameters.get('m_az')
         self.m_area = self._model.parameters.get('m_area')
         self.sim_start = self._model.parameters.get('sim_start')
+        self.input_type = self._model.parameters.get('input_type')
+        self.name = self._model.parameters.get('name')
+        self.par1 = self._model.parameters.get('par1')
+        self.par2 = self._model.parameters.get('par2')
         self.G_Gh = 0
         self.G_Dh = 0
         self.G_Bn = 0
@@ -99,6 +110,9 @@ class PV(ModelConstructor):
         self.sun_az = 0
         self.svf = 0
         self.g_aoi = 0
+
+        self.laplaceMax = laplace.pdf(0, scale=self.par2)
+        
         return result
 
 
@@ -121,6 +135,7 @@ class PV(ModelConstructor):
         """
         
         input_data = self.unpack_inputs(inputs)
+        """
         self.G_Gh = input_data['G_Gh']
         self.G_Dh = input_data['G_Dh']
         self.G_Bn = input_data['G_Bn']
@@ -128,13 +143,29 @@ class PV(ModelConstructor):
         self.hs = input_data['hs']
         self.FF = input_data['FF']
         self.Az = input_data['Az']
+        """
+        self.capacity_percentage = input_data['capacity_percentage']
 
-        results = self.output()
+        cap_percentage = self.addNoiseLaplace(self.capacity_percentage)
 
-        self.set_outputs({'pv_gen': results['pv_gen']})
-        self.set_states({'pv_genState': results['pv_gen']})
+        if self.input_type == 'irradiation':
+            results = self.output()
+        else:
+            pv_gen = cap_percentage * self.cap
+            results = {'pv_gen': pv_gen}
+
+        self.set_outputs({'pv_gen_out': results['pv_gen']})
+        self.set_states({'pv_gen': {self.name: results['pv_gen']}})
 
         return time + self._model.time_step_size
+
+    def addNoiseLaplace(self, input):
+        while True:
+            x = np.random.uniform(0, 10)
+            y = np.random.uniform(0, self.laplaceMax)
+            if y < laplace.pdf(x, loc=self.par1, scale=self.par2):
+                break
+        return max(min(input * x, 1), 0)
 
 
     def sun_azimuth(self):  # need to load sun_az
