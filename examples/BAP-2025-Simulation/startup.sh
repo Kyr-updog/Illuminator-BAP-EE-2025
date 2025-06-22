b4e804604a771bd23085af27ecac5569f8d42960
brammigamer <bramdorland1@gmail.com>
2025-06-22 21:48:50 +0200

    made startup files

---
+++ examples/BAP-2025-Simulation/startup.sh
@@ -0,0 +1,7 @@
+lxterminal -e ssh Raspinator@192.168.0.6 './Illuminator/configuration/runshfile/runRequester.sh 192.168.0.6 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.0.7 './Illuminator/configuration/runshfile/runRequester.sh 192.168.0.7 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.0.8 './Illuminator/configuration/runshfile/runRequester.sh 192.168.0.8 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.0.9 './Illuminator/configuration/runshfile/runRequester.sh 192.168.0.9 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.0.10 './Illuminator/configuration/runshfile/runRequester.sh 192.168.0.10 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.0.11 './Illuminator/configuration/runshfile/runRequester.sh 192.168.0.11 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.0.12 './Illuminator/configuration/runshfile/runRequester.sh 192.168.0.12 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
