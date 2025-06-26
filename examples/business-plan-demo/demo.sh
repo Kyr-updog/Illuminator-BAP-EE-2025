615650dbc2645659e8b39c6db743338fb8467ea7
Bram <bramdorland1@gmail.com>
2025-06-26 13:32:44 +0200

    shell file typo

--- examples/business-plan-demo/demo.sh
+++ examples/business-plan-demo/demo.sh
@@ -1,5 +1,5 @@
 lxterminal -e ssh Raspinator@192.168.0.6 './Illuminator/configuration/runshfile/runPV.sh 192.168.0.6 5100 /home/Raspinator/Illuminator/src/illuminator/models/'&
-lxterminal -e ssh Raspinator@192.168.0.10 './Illuminator/configuration/runshfile/runbattery.sh 192.168.0.10 5100 /home/Raspinator/Illuminator/src/illuminator/models/'&
+lxterminal -e ssh Raspinator@192.168.0.10 './Illuminator/configuration/runshfile/runBattery.sh 192.168.0.10 5100 /home/Raspinator/Illuminator/src/illuminator/models/'&
 lxterminal -e ssh Raspinator@192.168.0.11 './Illuminator/configuration/runshfile/runLoad.sh 192.168.0.11 5100 /home/Raspinator/Illuminator/src/illuminator/models/'&
 lxterminal -e ssh Raspinator@192.168.0.9 './Illuminator/configuration/runshfile/runWind.sh 192.168.0.9 5100 /home/Raspinator/Illuminator/src/illuminator/models/'&
 lxterminal -e ssh Raspinator@192.168.0.6 './Illuminator/configuration/runshfile/runLED_connection.sh 192.168.0.6 5000 /home/Raspinator/Illuminator/src/illuminator/models/'&
