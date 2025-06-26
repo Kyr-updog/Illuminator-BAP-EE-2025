b3abf57cd1f38501b3cfd500994e55b8bdafa82a
Bram <bramdorland1@gmail.com>
2025-06-26 13:31:10 +0200

    more typos

--- src/illuminator/models/__init__.py
+++ src/illuminator/models/__init__.py
@@ -65,5 +65,5 @@ __all__ = [ 'Adder',
             'PandaController',
             'Station',
             'WindRandomizer',
-            'business_controller'
+            'businessController'
             ]
